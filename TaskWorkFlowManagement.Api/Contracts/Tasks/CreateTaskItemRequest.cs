79fb9ec3dff2b3b16aeaed7f42dbb085688260cf
FacundoMartinez <facundo.eet2@gmail.com>
2026-06-15 20:35:21 -0300

    chore: validate task item titles and statuses

--- TaskWorkFlowManagement.Api/Contracts/Tasks/CreateTaskItemRequest.cs
+++ TaskWorkFlowManagement.Api/Contracts/Tasks/CreateTaskItemRequest.cs
@@ -1,8 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+using TaskWorkFlowManagement.Api.Models;
+
 namespace TaskWorkFlowManagement.Api.Contracts.Tasks;
 
-public class CreateTaskItemRequest
+public class CreateTaskItemRequest : IValidatableObject
 {
+    [Required]
+    [StringLength(TaskItem.TitleMaxLength)]
     public string Title { get; set; } = string.Empty;
 
     public string? Description { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (string.IsNullOrWhiteSpace(Title))
+        {
+            yield return new ValidationResult(
+                "Title is required.",
+                new[] { nameof(Title) });
+        }
+    }
 }
